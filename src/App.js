6cb17914c05f9a894713d30698dc07458ec9ad96
vanmathi291 <vanmathi291@gmail.com>
2023-03-22 12:25:13 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <div className="container">
+      <div className="data">HELLO</div>
+      <div className="data">WORLD</div>
+      <div className="data">HOW R U</div>
+     </div>
+     <div className="disk">
+     <Button as="a" variant="success">
+    Button as link
+  </Button>
+  </div>
+     </div>
   );
 }
 
---
+++ src/Demo1.js
@@ -0,0 +1,17 @@
+
+
+const Demo1 =()=>{
+    return(
+        <div className="project">
+            <div>
+                Server Migration
+                <br />
+                
+            </div>
+            <div></div>
+            Sales Tracking
+        </div>
+
+    )
+}
+export default Demo1
